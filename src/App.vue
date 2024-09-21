fccc9fbb9f7aa518648fb53d2921a38a593b0fbe
Elena Moreva <lenka.moreva@gmail.com>
2024-09-21 12:26:10 +0300

    Updates

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <template>
   <v-app>
     <v-main>
-      <router-view />      
+          <router-view /> 
+          <Lines></Lines>     
     </v-main>
   </v-app>
 </template>
 
 <script setup>
+import Lines from "./pages/"
 </script>
